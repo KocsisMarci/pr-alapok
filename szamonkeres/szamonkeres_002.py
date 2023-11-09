3caa8cc9ccc797ab9e0164863d2dfef15d83bebf
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2023-11-09 09:32:51 +0100

    Add files via upload

---
+++ szamonkeres/szamonkeres_002.py
@@ -0,0 +1,12 @@
+#Kocsis Marcell 10.D 1.csoport
+"""
+Bekérek egy egész számot, irassuk ki, hogy a szám pozitív negatív vagy netán 0!
+"""
+
+szam=int(input("Írj be egy számot: "))
+if szam>0:
+    print("Az általad beírt szám pozitív")
+if szam<0:
+    print("Az általad beírt szám negatív")
+if szam==0:
+    print("Az általad beírt szám 0")
