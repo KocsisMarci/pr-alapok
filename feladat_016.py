816005723ec58de8d82fce89bd48be4b1bbddea1
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2023-11-11 22:16:16 +0100

    Add files via upload

---
+++ feladat_016.py
@@ -0,0 +1,16 @@
+# While ciklus III.
+folytatja = True
+
+while folytatja:
+
+    print('Vidd ki a szemetet!')
+
+    valasz = input('Mondjam még egyszer? (i/n): ')
+
+    if valasz == 'n':
+
+        folytatja = False 
+
+
+
+print(f"A program vége")
