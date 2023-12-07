e9e4cc0409cbf14de06ec3c446f5ac504db02254
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2023-12-07 10:34:01 +0100

    Add files via upload

---
+++ feladat/feladat_031.py
@@ -0,0 +1,4 @@
+honapok=["Január","Február","Március","Április","Május","Június"]
+for inex in range(len(honapok)):
+        # az eredeti listaemlem módosul!
+        honapok[index]=honapok[index].upper()
