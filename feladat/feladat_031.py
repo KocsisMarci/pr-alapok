ac6a86e80d9a147831f85a310b210a9dbf96cae3
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2023-12-09 22:25:20 +0100

    Update feladat_031.py

--- feladat/feladat_031.py
+++ feladat/feladat_031.py
@@ -1,4 +1,4 @@
 honapok=["Január","Február","Március","Április","Május","Június"]
-for inex in range(len(honapok)):
+for index in range(len(honapok)):
         # az eredeti listaemlem módosul!
         honapok[index]=honapok[index].upper()
