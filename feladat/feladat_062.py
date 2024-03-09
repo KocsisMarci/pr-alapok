ea69d31f27b1e5314716ba16cd47be710a391cbf
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2024-03-09 23:22:29 +0100

    Add files via upload

---
+++ feladat/feladat_062.py
@@ -0,0 +1,17 @@
+def koszont():
+        print('Üdv a fedélzeten!')
+
+koszont()
+
+
+def koszont_nevvel(nev):
+        print('Szia '+ nev +', üdv a fedélzeten!')
+
+koszont_nevvel('Ádám')
+
+
+
+def koszont_ket_nevvel(nev1, nev2):
+        print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+
+koszont_ket_nevvel('Nóra', 'Ádám')
