e5f9f51658a29eb19f1baaa4f5a176e6e59b06cf
KocsisMarci <125395925+KocsisMarci@users.noreply.github.com>
2024-04-03 08:29:41 +0200

    Add files via upload

---
+++ kiiras3.py
@@ -0,0 +1,6 @@
+#Olvasás - írás
+
+with open("gyumolcsok.txt", "r", encoding="utf-8") as forrasfajl, \
+    open("gyumolcsok_masolata.txt","w",encoding="utf-8") as celfajl:
+    for sor in forrasfajl:
+        print(sor.strip(),file=celfajl)
